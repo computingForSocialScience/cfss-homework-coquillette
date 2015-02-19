893ca0570d5990da259cb506cc5a86a871638dca
Cici Coquillette <coquillette@uchicago.edu>
2015-02-18 20:13:40 -0600

    Passed Tests

--- Assignment6/analyzeNetworks.py
+++ Assignment6/analyzeNetworks.py
@@ -16,10 +16,11 @@ def readEdgeList(filename):
 #readEdgeList(filename)
 
 def degree(edgeList, in_or_out):
-	if in_or_out == 'in':
-		df = pd.DataFrame(edgeList['artist'].value_counts())
-	elif in_or_out == 'out':
-		df = pd.DataFrame(edgeList['related_artist'].value_counts())
+	#print len(edgeList)
+	if in_or_out == 'out':
+		df = edgeList['artist'].value_counts()
+	elif in_or_out == 'in':
+		df = edgeList['related_artist'].value_counts()
 	else:
 		print "you have to tell it in or out, stupid"
 	return df
@@ -29,23 +30,34 @@ def degree(edgeList, in_or_out):
 #edgeList = readEdgeList(filename)
 #print degree(edgeList, in_or_out)
 
-def combineEdgelists(edgeList1, edgeList2):
+def combineEdgeLists(edgeList1, edgeList2):
 	pieces = [edgeList1, edgeList2]
 	concatenated = pd.concat(pieces)
 	combined = concatenated.drop_duplicates()
 	#combined = DataFrame.drop_duplicates(concatenated)
 	return combined
 
-def pandastoNetworkX(edgeList): 
+def pandasToNetworkX(edgeList): 
 	g = nx.DiGraph()
-	for artist,related_artist in edgeList.to_records(index=False):
-		g.add_edge(artist,related_artist)
+	for artist, related_artist in edgeList.to_records(index=False):
+		g.add_edge(artist, related_artist)
 	return g
 
-def randomCentralNode(inputDiGraph):
+"""def randomCentralNode(inputDiGraph):
 	centrality_dict = nx.eigenvector_centrality(inputDiGraph)
 	normalization = float(sum(centrality_dict.itervalues()))
 	for key, value in centrality_dict.items():
-		centrality_dict[key] = value / normalization
+		centrality_dict[key] = value #/ normalization
+	random_node = np.random.choice(centrality_dict.keys()#, p=centrality_dict.values())
+	return random_node"""
+
+def randomCentralNode(inputDiGraph):
+	centrality_dict = nx.eigenvector_centrality(inputDiGraph)
+	normalization = sum(centrality_dict.values())
+	for key in centrality_dict:
+		try:
+			centrality_dict[key] = centrality_dict[key]/float(normalization)
+		except ZeroDivisionError:
+			centrality_dict[key] = 1.0/len(centrality_dict)
 	random_node = np.random.choice(centrality_dict.keys(), p=centrality_dict.values())
 	return random_node
